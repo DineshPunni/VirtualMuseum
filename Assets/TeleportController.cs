a6e48a3975390dbf5a4f0f99522127b1a4f28dcf
Dinesh Punni <dinesh.punni@hotmail.de>
2018-03-17 20:40:56 +0100

    Savepoint before importing the Oculus Samples framework

---
+++ Assets/TeleportController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportController : MonoBehaviour {
+
+    bool firstClick;
+    float clickTimer = 0.5f;
+
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+
+
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            firstClick = true;
+        }
+
+     
+        
+
+    }
+
+
+
+    void ButtonMapping()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.Back))
+        {
+        }
+
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryTouchpad))
+        {
+        }
+
+        if (OVRInput.GetDown(OVRInput.Touch.PrimaryTouchpad))
+        {
+        }
+
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+        {
+        }
+    }
+
+}
